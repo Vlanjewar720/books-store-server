7e1acd8aed6ddd0d36313136797fb1f10f7d4b73
Vaibhav <lanjewarvaibhav56@gmail.com>
2023-06-29 19:42:24 +0530

    crud

---
+++ app.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const router = require("./routes/book-routes");
+const cors = require("cors");
+const app = express();
+
+// Middlewares
+app.use(express.json());
+app.use(cors());
+app.use("/books", router); // localhost:5000/books
+
+mongoose
+  .connect(
+    "mongodb+srv://lanjewarvaibhav56:VaibhavLanjewar12345@cluster0.phq7pcx.mongodb.net/?retryWrites=true&w=majority"
+  )
+  .then(() => console.log("Connected To Database"))
+  .then(() => {
+    app.listen(5000, ()=> {
+      console.log('Node API app is running on port 5000')
+      } );
+  })
+  .catch((err) => console.log(err));
+
+  
